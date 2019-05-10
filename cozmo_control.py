5c6fa986e2260cfe1bf71cc0e68d80bd63318173
cloppenheimer <cloppenheimer@gmail.com>
2019-05-10 16:36:12 -0400

    Update cozmo_control.py

--- cozmo_control.py
+++ cozmo_control.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2016 Anki, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License in the file LICENSE.txt or at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-'''Control Cozmo using a webpage on your computer.
-
-This example lets you control Cozmo by Remote Control, using a webpage served by Flask.
-'''
+# This is based on an example at https://github.com/anki/cozmo-python-sdk/blob/master/examples/apps/remote_control_cozmo.py
 
 import asyncio
 import io
